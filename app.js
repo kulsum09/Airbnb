01349a52e64b8c2cdaff304f3727a3cd36690c69
Kulsum <kulsumalam1@gmail.com>
2025-12-07 00:56:24 +0530

    update2

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const {listingSchema} = require("./schema.js");
+const {listingSchema, reviewSchema} = require("./schema.js");
 const Review = require("./models/review.js");
 
 
@@ -47,6 +47,16 @@ let {error} = listingSchema.validate(req.body);
   }
 };
 
+const validateReview= (req, res, next) => {
+let {error} = reviewSchema.validate(req.body);
+  if(error) {
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  } else{
+    next();
+  }
+};
+
 //Index Route
 app.get("/listings", wrapAsync (async (req, res) => {
   const allListings = await Listing.find({});
@@ -62,7 +72,7 @@ app.get("/listings/new", (req, res) => {
 //Show Route
 app.get("/listings/:id", wrapAsync (async (req, res) => {
   let { id } = req.params;
-  const listing = await Listing.findById(id);
+  const listing = await Listing.findById(id).populate("reviews");
   res.render("listings/show.ejs", { listing });
 })
 );
@@ -110,7 +120,7 @@ app.delete("/listings/:id", wrapAsync (async (req, res) => {
 
 // reviews
 //post route
-app.post("/listings/:id/reviews", async(req, res) => {
+app.post("/listings/:id/reviews", validateReview, wrapAsync(async(req, res) => {
   let listing = await Listing.findById(req.params.id);
   let newReview = new Review(req.body.review);
 
@@ -119,7 +129,8 @@ app.post("/listings/:id/reviews", async(req, res) => {
   await listing.save();
 
  res.redirect(`/listings/${listing._id}`);
-});
+})
+);
 // app.get("/testListing", async (req, res) => {
 //   let sampleListing = new Listing({
 //     title: "My New Villa",
