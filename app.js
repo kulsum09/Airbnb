951046b58226d75ea8f5a87799e569e02888a819
Kulsum <kulsumalam1@gmail.com>
2025-12-11 10:07:44 +0530

    update3

--- app.js
+++ app.js
@@ -131,6 +131,17 @@ app.post("/listings/:id/reviews", validateReview, wrapAsync(async(req, res) => {
  res.redirect(`/listings/${listing._id}`);
 })
 );
+
+
+// delete review route
+app.delete("/listings/:id/reviews/:reviewId", wrapAsync(async (req, res) => {
+  let { id, reviewId } = req.params;
+  await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
+  await Review.findByIdAndDelete(reviewId);
+
+  res.redirect(`/listings/${id}`);
+})
+);
 // app.get("/testListing", async (req, res) => {
 //   let sampleListing = new Listing({
 //     title: "My New Villa",
