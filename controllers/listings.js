b9a3288b161f31caefa4e0a304f998242a780fdf
Kulsum <kulsumalam1@gmail.com>
2025-12-28 12:48:13 +0530

    upadate13

--- controllers/listings.js
+++ controllers/listings.js
@@ -37,15 +37,27 @@ module.exports.createListing = async (req, res, next) => {
   limit: 1,
   })
   .send();
-  console.log(response.body.features[0].geometry);
-  res.send("done!");
+
+  // 🛡️ SAFETY CHECK
+  if (!response.body.features.length) {
+    req.flash("error", "Invalid location. Please enter a valid place.");
+    return res.redirect("/listings/new");
+  }
+
   
   let url = req.file.path;
   let filename = req.file.filename;
   const newListing = new Listing(req.body.listing);
-  newListing.owner = req.user._id;
-  newListing.image = {url, filename};
-  await newListing.save();
+  
+  newListing.geometry = response.body.features[0].geometry;
+   newListing.owner = req.user._id;
+  newListing.image = {
+    url: req.file.path,
+    filename: req.file.filename,
+  };
+
+  let savedListing = await newListing.save();
+  console.log(savedListing);
   req.flash("success", "New Listing Created!");
   res.redirect("/listings");
 
@@ -66,14 +78,36 @@ module.exports.renderEditForm = async (req, res) => {
 
 module.exports.updateListing = async (req, res) => {
   let { id } = req.params;
-  let listing = await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  let listing = await Listing.findByIdAndUpdate(id, { ...req.body.listing } , {new: true});
    
-  if(typeof req.file !== "undefined") {
-  let url = req.file.path;
-  let filename = req.file.filename;
-  listing.image = {url, filename};
-  await listing.save();
+if (req.body.listing.location) {
+    const response = await geocodingClient
+      .forwardGeocode({
+        query: req.body.listing.location,
+        limit: 1,
+      })
+      .send();
+
+    if (response.body.features.length) {
+      listing.geometry = response.body.features[0].geometry;
+    }
   }
+
+  if (req.file) {
+    listing.image = {
+      url: req.file.path,
+      filename: req.file.filename,
+    };
+  }
+
+  // if(typeof req.file !== "undefined") {
+  // let url = req.file.path;
+  // let filename = req.file.filename;
+  // listing.image = {url, filename};
+  // await listing.save();
+  // }
+
+   await listing.save();
   req.flash("success", "Listing Updated!");
   res.redirect(`/listings/${id}`);
   
--- public/js/map.js
+++ public/js/map.js
@@ -1,7 +1,38 @@
-   mapboxgl.accessToken = mapToken;
-    const map = new mapboxgl.Map({
-        container: 'map', // container ID
-        style: 'mapbox://styles/mapbox/streets-v12',
-        center: [-74.5, 40], // starting position [lng, lat]. Note that lat must be set between -90 and 90
-        zoom: 9 // starting zoom
-    });
+//    mapboxgl.accessToken = mapToken;
+//     const map = new mapboxgl.Map({
+//         container: 'map', // container ID
+//         style: 'mapbox://styles/mapbox/streets-v12',
+//         center: coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
+//         zoom: 9 // starting zoom
+//     });
+
+//      const marker = new mapboxgl.Marker()
+//        .setLngLat(coordinates) //listing.geometry.coordinates
+//         .addTo(map);
+
+mapboxgl.accessToken = mapToken;
+
+if (
+  listing &&
+  listing.geometry &&
+  listing.geometry.coordinates &&
+  listing.geometry.coordinates.length === 2
+) {
+  const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v12',
+    center: listing.geometry.coordinates,
+    zoom: 9
+  });
+
+  new mapboxgl.Marker({ color: "red" })
+    .setLngLat(listing.geometry.coordinates)
+    .setPopup(
+      new mapboxgl.Popup({ offset: 25 })
+        .setHTML(`<h4>${listing.location}</h4><p>Welcome to Wanderlust!</p>`)
+    )
+    .addTo(map);
+
+} else {
+  console.error("Invalid or missing coordinates:", listing.geometry.coordinates);
+}
